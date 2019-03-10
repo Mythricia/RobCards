dd9b298c0b999d8c22a24e59056ba704e5c93a03
Mythricia <mythricia@gmail.com>
2019-03-10 02:48:50 +0100

    More garbage testing

--- main.lua
+++ main.lua
@@ -30,22 +30,29 @@ local actionTable = {
 
 
 local canvas
-local fonts = {}
+local font
 local cmode = "normal"
+local baseline
+local image
 
 function love.load()
 	InputManager.PushActionTable(actionTable)
 	love.graphics.setBackgroundColor(0, 161/255, 241/255)
 
+	image = love.graphics.newImage("/art/test.png")
 
 	canvas = love.graphics.newCanvas(400, 600, {msaa = 8, format=cmode})
-     -- Rectangle is drawn to the canvas with the regular alpha blend mode.
-    love.graphics.setCanvas(canvas)
-        love.graphics.clear()
-        love.graphics.setBlendMode("alpha")
-        love.graphics.setColor(1, 0, 0)
-        love.graphics.rectangle('fill', 0, 0, 400, 600, 50)
+	-- Rectangle is drawn to the canvas with the regular alpha blend mode.
+	love.graphics.setCanvas(canvas)
+	love.graphics.clear()
+	love.graphics.setBlendMode("alpha")
+	love.graphics.setColor(0.75, 0, 0)
+	--love.graphics.rectangle('fill', 0, 0, 400, 600, 50)
+	love.graphics.draw(image, 0,0,400,600)
 	love.graphics.setCanvas()
+
+	font = love.graphics.setNewFont(24)
+	baseline = font:getBaseline()
 end
 
 
@@ -53,21 +60,36 @@ function love.update(dt)
 end
 
 function love.draw()
-	--love.graphics.setColor(0.75,0,0)
-
 	-- very important!: reset color before drawing to canvas to have colors properly displayed
-    -- see discussion here: https://love2d.org/forums/viewtopic.php?f=4&p=211418#p211418
-    love.graphics.setColor(1, 1, 1, 1)
+	-- see discussion here: https://love2d.org/forums/viewtopic.php?f=4&p=211418#p211418
+	love.graphics.setColor(1, 0, 0, 0.75)
+	love.graphics.draw(image, 0,0,0)
 
-    -- The rectangle from the Canvas was already alpha blended.
-    -- Use the premultiplied alpha blend mode when drawing the Canvas itself to prevent improper blending.
-    love.graphics.setBlendMode("alpha", "premultiplied")
+
+	--[[
+	-- The rectangle from the Canvas was already alpha blended.
+	-- Use the premultiplied alpha blend mode when drawing the Canvas itself to prevent improper blending.
+	love.graphics.setBlendMode("alpha", "premultiplied")
 	love.graphics.draw(canvas, centerX-200, centerY-300)
 
+	-- Prepare for text
 	love.graphics.setBlendMode("alpha")
-	love.graphics.printf("Some Darn Text", 0, centerY, love.graphics.getWidth(), 'center')
+	local cy = centerY+baseline+2
 
-	love.graphics.polygon("fill", 100,100, 150,150, 100,200, 50,150, 100,100)
+	love.graphics.setColor(0,0,0,0.75)
+	love.graphics.line(centerX-200, cy, centerX+200, cy)
+	love.graphics.line(centerX-200, cy+50, centerX+200, cy+50)
+	love.graphics.line(centerX-200, cy+100, centerX+200, cy+100)
+	love.graphics.line(centerX-200, cy+150, centerX+200, cy+150)
+
+	love.graphics.setColor(0.75,0.75,0.75,1)
+	love.graphics.printf("Centered Text", centerX-200, centerY, 400, 'center')
+	love.graphics.printf("Spread Even Text", centerX-200, centerY+50, 400, 'justify')
+	love.graphics.printf("Left Text", centerX-200, centerY+100, 400, 'left')
+	love.graphics.printf("Right Text", centerX-200, centerY+150, 400, 'right')
+
+	love.graphics.printf("Centered Very Long And Probably Wrapping Text", centerX-200, centerY+200, 400, 'center')
+	]]
 end
 
 function love.mousepressed(x, y, button)
