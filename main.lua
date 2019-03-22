f3f834beb79c1c4df9dde2f3840f00887fe73710
Mythricia <mythricia@gmail.com>
2019-03-22 15:42:25 +0100

    * Refactor love.draw contents * Added key shortcut helper

--- main.lua
+++ main.lua
@@ -2,16 +2,16 @@
 local InputManager = require("modules.InputManager")
 local actionTable
 
+-- Forward function decs
+local drawKeyHelper
+local drawMouseHelper
+
 local centerX = love.graphics.getWidth() / 2
 local centerY = love.graphics.getHeight() / 2
 
-
-local canvas
-local font
-local cmode = "normal"
-local baseline
 local error
 local cards = {}
+local drawKeys = true
 
 local imageIndex = 1
 
@@ -59,25 +59,31 @@ function love.draw()
 		return
 	end
 
-	love.graphics.setColor(1, 1, 1, 1)
-	local drawCardX = centerX-(cards[imageIndex].image:getWidth() / 2)
-	local drawCardY = centerY-(cards[imageIndex].image:getHeight() / 2)
+	local cardWidth, cardHeight = cards[imageIndex].image:getDimensions()
+	local drawCardX = centerX-(cardWidth / 2)
+	local drawCardY = centerY-(cardHeight / 2)
 
-	love.graphics.draw(cards[imageIndex].image, drawCardX, drawCardY)
+	do -- Card drawing section
+		-- draw card bounds outline
+		love.graphics.setColor(0,1,0,0.25)
+		love.graphics.rectangle("line", drawCardX, drawCardY, cardWidth, cardHeight)
 
-	local indexText = (imageIndex.."/"..#cards.." ("..cards[imageIndex].metadata.name..")")
-	love.graphics.print(indexText, 15,15)
+		love.graphics.setColor(1, 1, 1, 1)
+		love.graphics.draw(cards[imageIndex].image, drawCardX, drawCardY)
 
+		local indexText = (imageIndex.."/"..#cards.." ("..cards[imageIndex].metadata.name..")")
+		love.graphics.print(indexText, 15,15)
+	end
 
+	-- Card coord helper
 	if love.mouse.isDown(1) then
-		local mouseX, mouseY = love.mouse.getPosition()
+		drawMouseHelper(drawCardX, drawCardY, cardWidth, cardHeight)
+	end
 
-		local cardX = mouseX - drawCardX
-		local cardY = mouseY - drawCardY
 
-		love.graphics.setColor(1,0,0,1)
-		love.graphics.circle("fill", mouseX, mouseY, 3)
-		love.graphics.print("Card Coords: "..cardX..","..cardY, mouseX+15, mouseY+15, 0, 1.5)
+	-- Draw keybindings, if drawKeys is true
+	if drawKeys then
+		drawKeyHelper()
 	end
 end
 
@@ -91,7 +97,6 @@ function love.wheelmoved(x, y)
 end
 
 
-
 actionTable = {
 	name = "default",
 
@@ -99,19 +104,22 @@ actionTable = {
 	{
 		onKeyDown = function()
 			love.event.quit()
-		end
+		end,
+		label = "Quit"
 	},
 	['escape'] =
 	{
 		onKeyDown = function()
 			love.event.quit()
-		end
+		end,
+		label = "Quit"
 	},
 	['r'] =
 	{
 		onKeyDown = function()
 			love.event.quit("restart")
-		end
+		end,
+		label = "Reload"
 	},
 	['space'] =
 	{
@@ -119,20 +127,87 @@ actionTable = {
 			print("Saving card to disk.....")
 			cards[imageIndex].imagedata:encode("png", cards[imageIndex].metadata.name..".png")
 			love.system.openURL(love.filesystem.getSaveDirectory())
-		end
+		end,
+		label = "Save to disk"
 	},
 	['right'] =
 	{
 		onKeyDown = function()
 			imageIndex = imageIndex + 1
 			if imageIndex > #cards then imageIndex = 1 end
-		end
+		end,
+		label = "Browse ->"
 	},
 	['left'] =
 	{
 		onKeyDown = function()
 			imageIndex = imageIndex - 1
 			if imageIndex < 1 then imageIndex = #cards end
-		end
+		end,
+		label = "Browse <-"
 	},
+	['h'] =
+	{
+		onKeyDown = function()
+			drawKeys = not drawKeys
+		end,
+		label = "Toggle this help"
+	},
+	['Left Click (Hold)'] =
+	{ -- Fake keybind, doesn't work with InputManager
+	label = "Card coordinates"}
 }
+
+
+function drawKeyHelper()
+	love.graphics.setColor(1,1,1,1)
+	local t = {}
+	for k,v in pairs(actionTable) do
+		if v.label then
+			t[v.label] = t[v.label] or {}
+			table.insert(t[v.label], k)
+		end
+	end
+
+
+	local n = 1
+	for k,v in pairs(t) do
+
+		local str = v[1]
+		if #v > 1 then
+			for i=2,#v do
+				str = str.." / "..v[i]
+			end
+		end
+
+		n = n + 1
+
+		str = string.upper(str)..": "..k
+		love.graphics.print(str, 10, love.graphics.getHeight()-20*n)
+	end
+end
+
+function drawMouseHelper(drawX, drawY, width, height)
+	local mouseX, mouseY = love.mouse.getPosition()
+
+		local cardX = math.clamp(mouseX - drawX, 0, width)
+		local cardY = math.clamp(mouseY - drawY, 0, height)
+		local str = "Card Coords: "..cardX..","..cardY
+
+		local font = love.graphics.newFont(20)
+		str = love.graphics.newText(font, str)
+
+		love.graphics.setColor(0,0,0,0.85)
+		love.graphics.rectangle("fill", mouseX-100, mouseY+17, 225, 19)
+
+		love.graphics.setColor(1,0,0,1)
+		love.graphics.circle("fill", mouseX, mouseY, 3)
+		love.graphics.draw(str, mouseX-100, mouseY+15)
+end
+
+
+function math.clamp(val, lower, upper)
+    assert(val and lower and upper, "math.Clamp:: Wrong number of arguments")
+    if lower > upper then lower, upper = upper, lower end -- swap if boundaries supplied the wrong way
+    return math.max(lower, math.min(upper, val))
+end
